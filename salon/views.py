b0703cc104a0773cd19344030ca05758c5d58cd3
ElenaPopD <popelena2689@gmail.com>
2024-02-24 21:43:39 +0200

    changed the reservation logic

--- salon/views.py
+++ salon/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 from django.http import HttpResponse  # noqa: F401
-from .models import  DescriereServicii
+from django.contrib import messages  # Adaugă această linie
+from .models import DescriereServicii
 from django.db.models import F 
 from .forms import ProgramareForm
 
@@ -14,19 +15,31 @@ def servicii(request):
     servicii = DescriereServicii.objects.all()
     return render(request, 'servicii.html', {'servicii': servicii})
 
-
 def programare(request):
     if request.method == 'POST':
         form = ProgramareForm(request.POST)
         if form.is_valid():
             form.save()
-            send_confirmation_email(form.instance, 'Programare înregistrată', 'Programare înregistrată cu succes! Vei primi un email de confirmare în cel mai scurt timp posibil!')
+            subiect_email = "🌙 Programare înregistrată la MoonlightRelax salon"
+            mesaj_email = """\
+Felicitări! Rezervarea ta la salonul Moonlight Relax a fost înregistrată cu succes.
+
+Ne bucurăm să te avem oaspete și suntem pregătiți să îți oferim o experiență de neuitat, plină de relaxare și reînnoire.
+
+Vei primi în curând un email de confirmare cu toate detaliile necesare. Te rugăm să verifici și folderul de spam, pentru a te asigura că primești toate informațiile importante.
+
+Îți mulțumim pentru încrederea acordată și abia așteptăm să te întâmpinăm în sanctuarul nostru de relaxare.
+
+Cu drag,
+Echipa Moonlight Relax
+"""
+            send_confirmation_email(form.instance, subiect_email, mesaj_email)
+            # Adaugă un mesaj care să fie afișat utilizatorului
+            messages.success(request, 'Rezervarea a fost înregistrată cu succes. Verifică adresa de email pentru confirmare.')
             return redirect('programare')
     else:
         form = ProgramareForm() 
     return render(request, 'programare.html', {'form': form})
-        
-
 
 def send_confirmation_email(programare, subiect_email, mesaj_email):
     print(programare)
@@ -35,4 +48,3 @@ def send_confirmation_email(programare, subiect_email, mesaj_email):
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [programare.email, ]
     send_mail(subject, message, email_from, recipient_list)
-    return HttpResponse('Email trimis cu succes!')  # noqa: F841
