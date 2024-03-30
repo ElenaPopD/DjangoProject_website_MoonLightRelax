d97fd5b2cd2c178abb97a339a8c14cfcce0cb07a
ElenaPopD <popelena2689@gmail.com>
2024-03-30 14:22:39 +0200

    added mobile resposive css styles

--- salon/tests.py
+++ salon/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+# from django.test import pytest
 
-# Create your tests here.
+# from salon.models import Despre
+# from salon.models import ContactInfo
+# from salon.views import homepage
+# from salon.views import servicii
+# from salon.views import programare
+# from salon.views import send_confirmation_email
+# from salon.views import about
+# from salon.views import contact
+
+
+
+# def test_homepage():
+#     assert homepage() == {'is_homepage': True}
+
+# def test_servicii():
+#     assert servicii() == DescriereServicii.objects.all()
+
+# def test_programare():
+#     assert programare() == ProgramareForm()
+
+# def test_send_confirmation_email(): 
+#     assert send_confirmation_email() == send_mail(subject, message, email_from, recipient_list)
+
+# def test_about():
+#     assert about() == Despre.objects.all()
+
+# def test_contact():
+#     assert contact() == ContactInfo.objects.all()
+
+
