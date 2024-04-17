3c5e9605a938be0a5241e5916593b5cbb29bee17
ElenaPopD <popelena2689@gmail.com>
2024-04-17 18:21:55 +0300

    edited settings.py

--- moonlight_relax/settings.py
+++ moonlight_relax/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = ['moonlightrelax.onrender.com', 'localhost', '127.0.0.1' ]
 
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
